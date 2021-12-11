c738ed393dfba165b7acd23eb38a0782f95cdb2e
Jhon Dorado <jh400@gmail.com>
2021-12-10 20:35:26 -0500

    se aregla proyectos

--- modelos/proyecto/tipos.js
+++ modelos/proyecto/tipos.js
@@ -1,22 +1,19 @@
-import { gql} from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
 
 const tiposProyecto = gql`
   type Objetivo {
     _id: ID!
     descripcion: String!
     tipo: Enum_TipoObjetivo!
-  } 
-  
+  }
   input crearObjetivo {
     descripcion: String!
     tipo: Enum_TipoObjetivo!
   }
-
   input camposObjetivo {
     descripcion: String!
     tipo: Enum_TipoObjetivo!
   }
-
   input camposProyecto {
     nombre: String
     presupuesto: Float
@@ -26,56 +23,36 @@ const tiposProyecto = gql`
     fase: Enum_FaseProyecto
     lider: String
   }
-
- 
-  type Query{
-      
-      Proyectos: [Proyecto]
+  type Proyecto {
+    _id: ID!
+    nombre: String!
+    presupuesto: Float!
+    fechaInicio: Date!
+    fechaFin: Date!
+    estado: Enum_EstadoProyecto!
+    fase: Enum_FaseProyecto!
+    lider: Usuario!
+    objetivos: [Objetivo]
+    avances: [Avance]
+    inscripciones: [Inscripcion]
   }
-
+  type Query {
+    Proyectos: [Proyecto]
+  }
   type Mutation {
-      
-
-     
-
     crearProyecto(
       nombre: String!
       presupuesto: Float!
       fechaInicio: Date!
       fechaFin: Date!
-      estado: Enum_EstadoProyecto!
-      fase: Enum_FaseProyecto!
       lider: String!
       objetivos: [crearObjetivo]
     ): Proyecto
-
     editarProyecto(_id: String!, campos: camposProyecto!): Proyecto
-
     crearObjetivo(idProyecto: String!, campos: camposObjetivo!): Proyecto
-
     editarObjetivo(idProyecto: String!, indexObjetivo: Int!, campos: camposObjetivo!): Proyecto
-
     eliminarObjetivo(idProyecto: String!, idObjetivo: String!): Proyecto
   }
-
- 
-
-  type Proyecto{
-      _id: ID!
-      nombre: String!
-      presupuesto: Float!
-      fechaInicio: Date!
-      fechaFin: Date!
-      estado: Enum_EstadoProyecto!
-      fase: Enum_FaseProyecto!
-      lider: Usuario!
-      objetivos: [Objetivo]
-      avances: [Avance]
-      inscripciones: [Inscripcion]
-      
-  }
-  
-          
-        
 `;
-export { tiposProyecto};
+
+export { tiposProyecto };
