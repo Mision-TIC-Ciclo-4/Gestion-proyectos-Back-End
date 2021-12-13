a8fed947431d6940e7b9ab636b321dae26684449
Jhon Dorado <jh400@gmail.com>
2021-12-13 16:31:11 -0500

    se arregla editar y eliminar objetivos

---
+++ graphql/auth/resolvers.js
@@ -0,0 +1,73 @@
+import { UserModel } from "../../modelos/usuario/usuario.js";
+import bcrypt from 'bcrypt';
+import {generateToken} from '../../utils/tokenUtils.js';
+
+
+const resolversAutenticacion = {
+    Mutation: {
+        registro: async (parent, args) => {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(args.password, salt);
+            const usuarioCreado = await UserModel.create({ 
+                nombre: args.nombre,
+                apellido: args.apellido,
+                identificacion: args.identificacion,
+                correo: args.correo,
+                rol: args.rol,
+                password: hashedPassword,
+            });
+            console.log('usuario creado', usuarioCreado);
+            return {
+                token: generateToken({
+                    _id: usuarioCreado._id,
+                    nombre: usuarioCreado.nombre,
+                    apellido: usuarioCreado.apellido,
+                    identificacion: usuarioCreado.identificacion,
+                    correo: usuarioCreado.correo,
+                    rol: usuarioCreado.rol,
+                }),
+            };
+        },
+        login: async (parents, args) =>{
+            const usuarioEncontrado = await UserModel.findOne({ correo: args.correo });
+            if (await bcrypt.compare(args.password, usuarioEncontrado.password)){
+                return{
+                    token: generateToken({
+                        id: usuarioEncontrado._id,
+                        nombre: usuarioEncontrado.nombre,
+                        apellido: usuarioEncontrado.apellido,
+                        identificacion: usuarioEncontrado.identificacion,
+                        correo: usuarioEncontrado.correo,
+                        rol: usuarioEncontrado.rol,
+                    }),
+                };
+
+            }
+            
+            
+        },
+
+        refreshToken: async(parent, args, context) => {
+            console.log("CONTEXTO",context);
+            if(!context.userData){
+                return{ error: 'token no valido',
+            };
+            }
+            else{
+                return {
+                    token: generateToken({
+                      _id: context.userData._id,
+                      nombre: context.userData.nombre,
+                      apellido: context.userData.apellido,
+                      identificacion: context.userData.identificacion,
+                      correo: context.userData.correo,
+                      rol: context.userData.rol,
+                    }),
+                  };
+                }
+               
+              },
+            },
+          };
+          
+          export { resolversAutenticacion };
